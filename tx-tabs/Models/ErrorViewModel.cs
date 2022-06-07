fa23d4fd4ca97ec35ce7058d56c9e085624f30c4
Bjoern Meyer <bjoern@textcontrol.com>
2022-06-07 14:27:42 +0200

    Add project files.

---
+++ tx-tabs/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace tx_tabs.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
